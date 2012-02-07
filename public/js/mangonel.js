85640b03820b487e04629197632897ed74f5c0e4
Fabrizio Codello <fabryz@gmail.com>
2012-02-07 11:20:43 +0100

    using Mangonel game handle

--- public/js/mangonel.js
+++ public/js/mangonel.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-	var game;
-
-	var canvas = $("#canvas");
+	var Mangonel;
 	
 	var fps = $("#fps"),
 		clientId = $("#clientId"),
@@ -12,71 +10,71 @@ $(document).ready(function() {
 	function sendMovement() {
 		var nowMove;
 
-		if (game.player.hasMoved()) {
+		if (Mangonel.player.hasMoved()) {
 			var dir = 'idle';
 
-			if (game.player.moveLeft) {
+			if (Mangonel.player.moveLeft) {
 				dir = 'l';
 			}
-			if (game.player.moveRight) {
+			if (Mangonel.player.moveRight) {
 				dir = 'r';
 			}
-			if (game.player.moveUp) {
+			if (Mangonel.player.moveUp) {
 				dir = 'u';
 			}
-			if (game.player.moveDown) {
+			if (Mangonel.player.moveDown) {
 				dir = 'd';
 			}
 
 			nowMove = Date.now();
-			if ((nowMove - game.player.lastMove) > game.allowSendEvery) { 
-				game.socket.emit('play', { id: game.player.id, dir: dir });
-				game.player.lastMove = Date.now();
+			if ((nowMove - Mangonel.player.lastMove) > Mangonel.allowSendEvery) { 
+				Mangonel.socket.emit('play', { id: Mangonel.player.id, dir: dir });
+				Mangonel.player.lastMove = Date.now();
 			}
 		}
 	}
 
 	function gameLoop() {
-		game.ctx.clearRect(0, 0, game.canvasWidth, game.canvasHeight);
+		Mangonel.ctx.clearRect(0, 0, Mangonel.canvasWidth, Mangonel.canvasHeight);
 		
-		if (game.isPlaying) {			
+		if (Mangonel.isPlaying) {			
 			sendMovement();
 
-			game.player.draw(game.ctx);
+			Mangonel.player.draw(Mangonel.ctx);
 
-			var length = game.players.length;
+			var length = Mangonel.players.length;
 			for(var i = 0; i < length; i++) {
-				if (game.players[i].id != game.player.id) {
-					game.players[i].draw(game.ctx);
+				if (Mangonel.players[i].id != Mangonel.player.id) {
+					Mangonel.players[i].draw(Mangonel.ctx);
 		    	}
 			}
 			
-			game.fps.count++;
+			Mangonel.fps.count++;
 
 			requestAnimationFrame(gameLoop);
 		}
 	}
 
 	function gameStart() {
-		if (game.isReady) {
-			game.debug('Ready! Starting.');
-			game.isPlaying = true;
+		if (Mangonel.isReady) {
+			Mangonel.debug('Ready! Starting.');
+			Mangonel.isPlaying = true;
 			
 			$(window).keydown(function(e) {
 				//e.preventDefault();
 
 				switch(e.keyCode) {
-					case game.keys.left:
-							game.player.moveLeft = true;
+					case Mangonel.keys.left:
+							Mangonel.player.moveLeft = true;
 						break;
-					case game.keys.right:
-							game.player.moveRight = true;
+					case Mangonel.keys.right:
+							Mangonel.player.moveRight = true;
 						break;
-					case game.keys.up:
-							game.player.moveUp = true;
+					case Mangonel.keys.up:
+							Mangonel.player.moveUp = true;
 						break;
-					case game.keys.down:
-							game.player.moveDown = true;
+					case Mangonel.keys.down:
+							Mangonel.player.moveDown = true;
 						break;
 
 					default:
@@ -95,21 +93,21 @@ $(document).ready(function() {
 				//e.preventDefault();
 				
 				switch(e.keyCode) {
-					case game.keys.left:
-							game.player.moveLeft = false;
+					case Mangonel.keys.left:
+							Mangonel.player.moveLeft = false;
 						break;
-					case game.keys.right:
-							game.player.moveRight = false;
+					case Mangonel.keys.right:
+							Mangonel.player.moveRight = false;
 						break;
-					case game.keys.up:
-							game.player.moveUp = false;
+					case Mangonel.keys.up:
+							Mangonel.player.moveUp = false;
 						break;
-					case game.keys.down:
-							game.player.moveDown = false;
+					case Mangonel.keys.down:
+							Mangonel.player.moveDown = false;
 						break;
 
-					case game.keys.backslash:
-							game.toggleDebugPanel();
+					case Mangonel.keys.backslash:
+							Mangonel.toggleDebugPanel();
 						break;
 
 					default:
@@ -117,34 +115,16 @@ $(document).ready(function() {
 				}
 			});
 
-			game.fps.init(fps);
+			Mangonel.fps.init(fps);
 
 			gameLoop();
 		} else {
-			console.log('Not ready.');
+			Mangonel.debug('Not ready.');
 		}
 	}
 
 	function gameInit() {
-		game = new Game();
-
-		game.player = new Player();
-		game.players = [];
-
-		game.canvas = canvas;
-		game.ctx = canvas.get(0).getContext("2d");
-		game.canvasWidth = canvas.width();
-		game.canvasHeight = canvas.height();
-
-		game.ctx.fillStyle = 'rgb(0, 0, 0)';
-		game.ctx.font = "15px Monospace";
-
-		game.fps = new Fps(2000);
-
-		game.socket = game.connect();
-		game.debug('Connecting...');
-
-		game.isReady = true;
+		Mangonel = new Game();
 	}
 
 	/*
@@ -157,65 +137,65 @@ $(document).ready(function() {
 	* Socket.io
 	*/
 	    
-    game.socket.on('connect', function() {
-    	game.debug('Connected.');
+    Mangonel.socket.on('connect', function() {
+    	Mangonel.debug('Connected.');
     	gameStart();
 	});
 			
-	game.socket.on('disconnect', function() {
-		game.debug('Disconnected.');
-		game.stop();
+	Mangonel.socket.on('disconnect', function() {
+		Mangonel.debug('Disconnected.');
+		Mangonel.stop();
 	});
 	
-	game.socket.on('clientId', function(data) {
+	Mangonel.socket.on('clientId', function(data) {
     	clientId.html(data.id);
 	});
 	
-	game.socket.on('tot', function(data) {	
+	Mangonel.socket.on('tot', function(data) {	
 		tot.html(data.tot);
 	});
 
 	/* Game logics */
 
-	game.socket.on('join', function(data) {						
-		game.player.id = data.player.id;
-		game.player.nick = data.player.nick;
-		game.player.x = data.player.x;
-		game.player.y = data.player.y;
+	Mangonel.socket.on('join', function(data) {						
+		Mangonel.player.id = data.player.id;
+		Mangonel.player.nick = data.player.nick;
+		Mangonel.player.x = data.player.x;
+		Mangonel.player.y = data.player.y;
 		
-		game.debug('Received current player id: '+ game.player.id);
-		game.debug('You have joined the server.');
+		Mangonel.debug('Received current player id: '+ Mangonel.player.id);
+		Mangonel.debug('You have joined the server.');
 	});
 
-	game.socket.on('quit', function(data) {
+	Mangonel.socket.on('quit', function(data) {
 		var quitter = '';
 
-		var length = game.players.length;
+		var length = Mangonel.players.length;
 		for(var i = 0; i < length; i++) {
-			if (game.players[i].id == data.id) {
-				quitter = game.players[i].nick;
-				game.players.splice(i, 1);
+			if (Mangonel.players[i].id == data.id) {
+				quitter = Mangonel.players[i].nick;
+				Mangonel.players.splice(i, 1);
 				break;
 			}
 		}
 		
-		game.debug('Player quitted: '+ quitter +' (id '+ data.id +')');
+		Mangonel.debug('Player quitted: '+ quitter +' (id '+ data.id +')');
 	});
 
-	game.socket.on('newplayer', function(data) {	
+	Mangonel.socket.on('newplayer', function(data) {	
 		var newPlayer = new Player();
 		newPlayer.id = data.player.id;
 		newPlayer.nick = data.player.nick;
 		newPlayer.x = data.player.x;
 		newPlayer.y = data.player.y;
 	
-		game.players.push(newPlayer);
-		game.debug('New player joined: '+ newPlayer.nick);
+		Mangonel.players.push(newPlayer);
+		Mangonel.debug('New player joined: '+ newPlayer.nick);
 		tmpPlayer = {};
 	});
 	
-	game.socket.on('playerlist', function(data) {				
-		game.players = []; //prepare for new list
+	Mangonel.socket.on('playerlist', function(data) {				
+		Mangonel.players = []; //prepare for new list
 
 		var length = data.list.length;
 		for(var i = 0; i < length; i++) {		
@@ -226,22 +206,22 @@ $(document).ready(function() {
 			tmpPlayer.y = data.list[i].y;
 			tmpPlayer.ping = data.list[i].ping;
 			
-			game.players.push(tmpPlayer);
+			Mangonel.players.push(tmpPlayer);
 			tmpPlayer = {};
 		}
 
-		game.debug('Initial player list received: '+ length +' players.');	
+		Mangonel.debug('Initial player list received: '+ length +' players.');	
 	});
 
-	game.socket.on('play', function(data) {
-		var length = game.players.length;
+	Mangonel.socket.on('play', function(data) {
+		var length = Mangonel.players.length;
 		for(var i = 0; i < length; i++) {
-			if (game.players[i].id == data.id) {
-				game.players[i].x = data.x;
-				game.players[i].y = data.y;
-				if (game.player.id == data.id) {
-					game.player.x = data.x;
-					game.player.y = data.y;
+			if (Mangonel.players[i].id == data.id) {
+				Mangonel.players[i].x = data.x;
+				Mangonel.players[i].y = data.y;
+				if (Mangonel.player.id == data.id) {
+					Mangonel.player.x = data.x;
+					Mangonel.player.y = data.y;
 				}
 			}
 		}
