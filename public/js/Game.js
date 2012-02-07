78a9a63fdd221b630a7a63aa576dd679f0507ebc
Fabrizio Codello <fabryz@gmail.com>
2012-02-07 11:20:00 +0100

    revised Game Class, exposing methods correctly

--- public/js/Game.js
+++ public/js/Game.js
@@ -1,60 +1,79 @@
 (function(exports) {
 
-	var Game = function () {
-		this.desiredFPS = 60;
-		this.allowSendEvery = 75; //TODO: tune this, 1/16s
+	var Game = function() {
+		var desiredFPS = 60,
+			allowSendEvery = 75,
+			isReady = true,
+			isPlaying = false;
+		
+		var	keys = {
+				up : 38,
+				down : 40,
+				left : 37,
+				right : 39,
+				tab : 9,
+				space : 32,
+				enter : 13,
+				w : 87,
+				s : 83,
+				a : 65,
+				d : 68,
+				backslash : 220
+			};
+		
+		var player = new Player(),
+			players = [];
 
-		this.isReady = false;
-		this.isPlaying = false;
+		var fps = new Fps(2000),
+			socket = new io.connect(window.location.href);
+		
+		var canvas = $('#canvas'),
+			ctx = canvas.get(0).getContext("2d"),
+			canvasWidth = canvas.width(),
+			canvasHeight = canvas.height();
 
-		this.keys = this.loadKeys();
-
-		this.debug('Game inited.');
-	};
-
-	Game.prototype.debug = function(msg) {
-		console.log(msg);
-	};
-
-	Game.prototype.loadKeys = function() {
-		return  {
-			up : 38,
-			down : 40,
-			left : 37,
-			right : 39,
-			tab : 9,
-			space : 32,
-			enter : 13,
-			w : 87,
-			s : 83,
-			a : 65,
-			d : 68,
-			backslash : 220
-		};
-	};
-
-	Game.prototype.connect = function() {
-		return new io.connect(window.location.href);
-	};
-
-	Game.prototype.init = function(canvas) {
-
-	};
-
-	Game.prototype.stop = function() {
-		this.isPlaying = false;
-	};
-
-	Game.prototype.toggleDebugPanel = function(spd) {
-		var speed = spd || 'fast';
+		ctx.fillStyle = 'rgb(0, 0, 0)';
+		ctx.font = "15px Monospace";
 	
-		$('#debug').fadeToggle(speed);
-		$('#debug').toggleClass("active");
-		if ($('#debug').hasClass("active")) {
+		var debug = function(msg) {
+			console.log(msg);
+		};
 
-		} else {
+		var stop = function() {
+			isPlaying = false;
+		};
 
-		}
+		var toggleDebugPanel = function(spd) {
+			var speed = spd || 'fast';
+		
+			$('#debug').fadeToggle(speed);
+			$('#debug').toggleClass("active");
+			if ($('#debug').hasClass("active")) {
+
+			} else {
+
+			}
+		};
+
+		return {
+			socket: socket,
+			keys: keys,
+			player: player,
+			players: players,
+			desiredFPS: desiredFPS,
+			allowSendEvery: allowSendEvery,
+			isReady: isReady,
+			isPlaying: isPlaying,
+			fps: fps,
+			canvas: canvas,
+			ctx: ctx,
+			canvasWidth: canvasWidth,
+			canvasHeight: canvasHeight,
+
+			debug: debug,
+			stop: stop,
+			toggleDebugPanel: toggleDebugPanel
+		};
 	};
 
 	exports.Game = Game;
