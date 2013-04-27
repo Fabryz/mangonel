bca7ed27ba6e99805a4f1493e2b233018839d767
Fabrizio Codello <fabryz@gmail.com>
2013-04-27 16:54:21 +0200

    Default nick = Player, default id = 0

--- public/js/Player.js
+++ public/js/Player.js
@@ -1,21 +1,21 @@
 (function(exports) {
 
 	var Player = function(id, x, y) {
-		this.id = id || -1;
-		this.nick = 'Guest'+ this.id;
+		this.id = id || 0;
+		this.nick = 'Player'+ this.id;
 		this.x = x || 0;
 		this.y = y || 0;
-		
+
 		this.isAlive = true;
 		this.HP = 100;
 		this.lastMoveTime = Date.now();
 		this.ping = 0;
-		
+
 		this.width = 10;
 		this.height = 10;
 		this.centerX = this.width / 2;
 		this.centerY = this.height / 2;
-		
+
 		this.moved = false;
 		this.moveLeft = false;
 		this.moveRight = false;
@@ -24,13 +24,13 @@
 		this.lastMoveDir = 'd';
 	};
 
-	Player.prototype.toString = function() { 
+	Player.prototype.toString = function() {
 		return /*this.id +' '+ */this.nick +' '+ this.x +':'+ this.y +' '+ this.ping +'ms';
 	};
 
 	Player.prototype.hasMoved = function() {
 		this.moved = (this.moveLeft || this.moveRight || this.moveUp || this.moveDown);
-			
+
 		return this.moved;
 	};
 
